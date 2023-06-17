ceb204cc18e273e5a03affeb5a1434cd179dc041
Jari Honkanen <jaarhonka@yahoo.com>
2023-06-17 09:31:23 +0300

    move ethernet receiver into own module

--- titanium_custom_board/cs_top.vhd
+++ titanium_custom_board/cs_top.vhd
@@ -2,16 +2,26 @@ library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
+    use work.ethernet_rx_ddio_pkg.all;
+    use work.PCK_CRC32_D8.all;
+    use work.ethernet_frame_ram_write_pkg.all;
+
 package ethernet_receiver_pkg is
 
     type ethernet_receiver_record is record
-        shift_register : std_logic_vector(15 downto 0);
-        crc32          : std_logic_vector(31 downto 0);
-        frame_detected : boolean;
+        shift_register       : std_logic_vector(15 downto 0);
+        crc32                : std_logic_vector(31 downto 0);
+        frame_detected       : boolean;
         receiver_ram_address : natural;
+        crc_counter          : natural range 0 to 7;
     end record;
 
-    constant init_ethernet_receiver : ethernet_receiver_record := ((others => '0'), (others => '1'), false, 0);
+    constant init_ethernet_receiver : ethernet_receiver_record := ((others => '0'), (others => '1'), false, 0, 0);
+
+    procedure create_ethernet_receiver (
+        signal self : inout ethernet_receiver_record;
+        enet_rx_ddio : in ethernet_rx_ddio_data_output_group;
+        signal ram_write : out ram_write_control_record);
 
     procedure idle_transmitter (
         signal ddio_hi, ddio_lo : out std_logic_vector(4 downto 0));
@@ -23,6 +33,44 @@ package ethernet_receiver_pkg is
 end package ethernet_receiver_pkg;
 
 package body ethernet_receiver_pkg is
+
+    procedure create_ethernet_receiver
+    (
+        signal self : inout ethernet_receiver_record;
+        enet_rx_ddio : in ethernet_rx_ddio_data_output_group;
+        signal ram_write : out ram_write_control_record
+    ) is
+    begin
+            if ethernet_rx_is_active(enet_rx_ddio) then
+                self.shift_register <= self.shift_register(7 downto 0) & get_byte(enet_rx_ddio);
+                if self.shift_register(7 downto 0) & get_byte(enet_rx_ddio) = x"aaab" then
+                    self.frame_detected <= true;
+                end if;
+                if self.frame_detected then
+                    self.crc32      <= nextCRC32_D8(get_byte(enet_rx_ddio), self.crc32);
+                end if;
+
+                if self.receiver_ram_address < 2**10-1 then
+                    self.receiver_ram_address <= self.receiver_ram_address + 1;
+                    if self.frame_detected then
+                        write_data_to_ram(ram_write, self.receiver_ram_address, get_byte_with_inverted_bit_order(enet_rx_ddio));
+                    else
+                        write_data_to_ram(ram_write, self.receiver_ram_address, get_byte(enet_rx_ddio));
+                    end if;
+                end if;
+                self.crc_counter <= 4;
+            else
+                self.frame_detected <= false;
+
+                if self.crc_counter > 0 then
+                    self.crc_counter <= self.crc_counter - 1;
+                    self.crc32 <= self.crc32(23 downto 0) & x"ff";
+                    write_data_to_ram(ram_write, self.receiver_ram_address, self.crc32(31 downto 24));
+                    self.receiver_ram_address <= self.receiver_ram_address + 1;
+                end if;
+            end if;
+        
+    end create_ethernet_receiver;
         procedure transmit_byte
         (
             signal ddio_hi, ddio_lo : out std_logic_vector(4 downto 0);
@@ -118,7 +166,6 @@ architecture rtl of top is
     signal crc_check_counter : natural range 0 to 2**16-1 := 0;
     
     signal empty_ram : boolean := false;
-    signal crc_counter : integer range 0 to 7 := 0;
 
     signal self : ethernet_receiver_record := init_ethernet_receiver;
 
@@ -193,36 +240,7 @@ begin
             end if;
 
             init_ram_write(write_port);
-            -- rmgii_active <= ethernet_rx_is_active(ethernet_ddio_out);
-            if ethernet_rx_is_active(ethernet_ddio_out) then
-                self.shift_register <= self.shift_register(7 downto 0) & get_byte(ethernet_ddio_out);
-                if self.shift_register(7 downto 0) & get_byte(ethernet_ddio_out) = x"aaab" then
-                    self.frame_detected <= true;
-                end if;
-                if self.frame_detected then
-                    self.crc32      <= nextCRC32_D8(get_byte(ethernet_ddio_out), self.crc32);
-                end if;
-
-                if self.receiver_ram_address < 2**10-1 then
-                    self.receiver_ram_address <= self.receiver_ram_address + 1;
-                    if self.frame_detected then
-                        write_data_to_ram(write_port, self.receiver_ram_address, get_byte_with_inverted_bit_order(ethernet_ddio_out));
-                    else
-                        write_data_to_ram(write_port, self.receiver_ram_address, get_byte(ethernet_ddio_out));
-                    end if;
-                end if;
-                crc_counter <= 4;
-            else
-                self.frame_detected <= false;
-
-                if crc_counter > 0 then
-                    crc_counter <= crc_counter - 1;
-                    self.crc32 <= self.crc32(23 downto 0) & x"ff";
-                    write_data_to_ram(write_port, self.receiver_ram_address, self.crc32(31 downto 24));
-                    self.receiver_ram_address <= self.receiver_ram_address + 1;
-                end if;
-            end if;
-
+            create_ethernet_receiver(self, ethernet_ddio_out, write_port);
 
             ------------------------------------------------------------------------
             output_shift_register <= output_shift_register(7 downto 0) & output_shift_register(15 downto 8);
